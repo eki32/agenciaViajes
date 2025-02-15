3b4c1de365b276b32bedc97d6d951d400e5643d7
eki32 <ekaitz.aguilar26@gmail.com>
2025-02-15 22:53:19 +0100

    Agrege un comentario de prueba

--- reto2/src/agenciaViajes/AgenciaViajesMain.java
+++ reto2/src/agenciaViajes/AgenciaViajesMain.java
@@ -17,6 +17,7 @@ public class AgenciaViajesMain extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	//probando cosas en repositorio github
+	//agrego un cometario desde la rama ekaitz
 
 	
 	/**
